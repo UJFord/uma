111b0d76d2fa0d406de06307900623f631dfd607
NoobGaming69 <noel.salazar15.es@gmail.com>
2024-01-01 22:49:50 +0800

    fixed the crop.php upto input date

--- admin/crop/crop.php
+++ admin/crop/crop.php
@@ -36,15 +36,15 @@ require('../sidebar/side.php');
 			<?php
 			if (isset($_GET['crop_id'])) {
 				$crop_id = pg_escape_string($connection, $_GET['crop_id']);
-				$query = "SELECT * from crop WHERE crop_id='$crop_id'";
-				$query_run = pg_query($connection, $query);
+				$query = "SELECT crop.*, crop_location.* FROM crop LEFT JOIN crop_location ON crop.crop_id = crop_location.crop_id WHERE crop.crop_id = $1";
+				$query_run = pg_query_params($connection, $query, array($crop_id));
 
 				$emptyValue = 'Empty';
 
 				if (pg_num_rows($query_run) > 0) {
 					$crops = pg_fetch_assoc($query_run);
-
 					// get the id for the roreign tables
+					$location_id = $crops['location_id'];
 					$current_crop_location_id = $crops['crop_location_id'];
 					$current_crop_farming_practice_id = $crops['crop_farming_practice_id'];
 					$current_crop_image = $crops['crop_image'];
@@ -54,19 +54,13 @@ require('../sidebar/side.php');
 					$crop_name = isset($crops['crop_name']) ? htmlspecialchars($crops['crop_name'], ENT_QUOTES) : $emptyValue;
 					$upland_or_lowland = isset($crops['upland_or_lowland']) ? htmlspecialchars($crops['upland_or_lowland'], ENT_QUOTES) : $emptyValue;
 					$category = isset($crops['category']) ? htmlspecialchars($crops['category'], ENT_QUOTES) : $emptyValue;
-					$description = isset($crops['description']) ? htmlspecialchars($crops['description'], ENT_QUOTES) : $emptyValue;
-					$image = isset($crops['image']) ? htmlspecialchars($crops['image'], ENT_QUOTES) : $emptyValue;
-					$local_name = isset($crops['local_name']) ? htmlspecialchars($crops['local_name'], ENT_QUOTES) : $emptyValue;
-					$planting_techniques = isset($crops['planting_techniques']) ? htmlspecialchars($crops['planting_techniques'], ENT_QUOTES) : $emptyValue;
-					$cultural_and_spiritual_significance = isset($crops['cultural_and_spiritual_significance']) ? htmlspecialchars($crops['cultural_and_spiritual_significance'], ENT_QUOTES) : $emptyValue;
-					$role_in_maintaining_upland_ecosystem = isset($crops['role_in_maintaining_upland_ecosystem']) ? htmlspecialchars($crops['role_in_maintaining_upland_ecosystem'], ENT_QUOTES) : $emptyValue;
-					$cultural_importance_and_traditional_knowledge = isset($crops['cultural_importance_and_traditional_knowledge']) ? htmlspecialchars($crops['cultural_importance_and_traditional_knowledge'], ENT_QUOTES) : $emptyValue;
-					$threats = isset($crops['threats']) ? htmlspecialchars($crops['threats'], ENT_QUOTES) : $emptyValue;
+					$crop_description = isset($crops['crop_description']) ? htmlspecialchars($crops['crop_description'], ENT_QUOTES) : $emptyValue;
+					$crop_variety = isset($crops['crop_variety']) ? htmlspecialchars($crops['crop_variety'], ENT_QUOTES) : $emptyValue;
+					$crop_origin = isset($crops['crop_origin']) ? htmlspecialchars($crops['crop_origin'], ENT_QUOTES) : $emptyValue;
+					$crop_scientific_name = isset($crops['crop_scientific_name']) ? htmlspecialchars($crops['crop_scientific_name'], ENT_QUOTES) : $emptyValue;
+					$crop_local_name = isset($crops['crop_local_name']) ? htmlspecialchars($crops['crop_local_name'], ENT_QUOTES) : $emptyValue;
 					$other_info = isset($crops['other_info']) ? htmlspecialchars($crops['other_info'], ENT_QUOTES) : $emptyValue;
-					$unique_features = isset($crops['unique_features']) ? htmlspecialchars($crops['unique_features'], ENT_QUOTES) : $emptyValue;
-					$cultural_use = isset($crops['cultural_use']) ? htmlspecialchars($crops['cultural_use'], ENT_QUOTES) : $emptyValue;
-					$associated_vegetation = isset($crops['associated_vegetation']) ? htmlspecialchars($crops['associated_vegetation'], ENT_QUOTES) : $emptyValue;
-					$last_seen_location = isset($crops['last_seen_location']) ? htmlspecialchars($crops['last_seen_location'], ENT_QUOTES) : $emptyValue;
+					$status = isset($crops['status']) ? htmlspecialchars($crops['status'], ENT_QUOTES) : $emptyValue;
 
 			?>
 					<!-- form for submitting -->
@@ -82,6 +76,7 @@ require('../sidebar/side.php');
 						<div class="form-control p-3 mt-3">
 							<input id="crop-id" type="hidden" name="crop_id" value="<?= $crops['crop_id']; ?>">
 							<input type="hidden" name="crop_location_id" value="<?= $crops['crop_location_id']; ?>">
+							<input type="hidden" name="crop_farming_practice_id" value="<?= $crops['crop_farming_practice_id']; ?>">
 							<input type="hidden" name="current_crop_image" value="<?= $current_crop_image; ?>">
 
 							<!-- general information -->
@@ -95,7 +90,7 @@ require('../sidebar/side.php');
 								<!-- image -->
 								<div class="col-4">
 									<label for="image-input" class="">Images <span class="text-danger">*</span></label>
-									<input type="file" class="form-control" name="image[]" id="image-input" multiple accept="image/*" hidden>
+									<input type="file" class="form-control" name="crop_image[]" id="image-input" multiple accept="image/*" hidden>
 								</div>
 							</div>
 
@@ -115,7 +110,7 @@ require('../sidebar/side.php');
 
 									<!-- local name -->
 									<label for="local">Local Name <span class="text-danger">*</span></label>
-									<input id="local" type="text" name="local_name" value="<?= $local_name ?>" class="form-control mb-2" disabled>
+									<input id="local" type="text" name="crop_local_name" value="<?= $crop_local_name ?>" class="form-control mb-2" disabled>
 									<!-- upland or lowland -->
 
 									<label>Type <span class="text-danger">*</span></label>
@@ -162,112 +157,72 @@ require('../sidebar/side.php');
 							<div class="col">
 								<!-- Description -->
 								<label for="gen-desc">Description <span class="text-danger">*</span></label>
-								<textarea name="description" id="gen-desc" class="form-control" rows="3" disabled <?php echo ($description !== $emptyValue) ? '>' . $description : 'placeholder="Empty">'; ?></textarea>
+								<textarea name="crop_description" id="gen-desc" class="form-control" rows="3" disabled <?php echo ($crop_description !== $emptyValue) ? '>' . $crop_description : 'placeholder="Empty">'; ?></textarea>
 							</div>
 
-							<!-- Characteristics -->
-							<h3 class="mt-4">Characteristics</h5>
-								<?php
-								// PHP code to display available Traditional Crop Traits from the database
+							<!-- More -->
+							<h3 class="mt-4">More</h5>
 
-								// Query to select all available Traditional Crop Traits in the database
-								$query3 = "SELECT * FROM traditional_crop_traits WHERE traditional_crop_traits_id='$current_traditional_crop_traits_id'";
+							<!-- Location -->
+							<h3 class="mt-4">Location</h5>
+								<?php
+								// PHP code to display available Location from the database
+
+								// Query to select all available Location in the database
+								$query3 = "SELECT crop_location.*, location.* FROM crop_location left join location on crop_location.location_id = location.location_id WHERE location.location_id='$location_id'";
 
 								// Executing query
 								$query_run3 = pg_query($connection, $query3);
 
-								// If count is greater than 0, we have Traditional Crop Traits; else, we do not have Traditional Crop Traits
+								// If count is greater than 0, we have Location; else, we do not have Location
 								if (pg_num_rows($query_run3) > 0) {
-									$traditional_crop_traits = pg_fetch_assoc($query_run3);
+									$location = pg_fetch_assoc($query_run3);
 
 									// Define default values for each field if they are $emptyValue
-									$taste = isset($traditional_crop_traits['taste']) ? $traditional_crop_traits['taste'] : $emptyValue;
-									$aroma = isset($traditional_crop_traits['aroma']) ? $traditional_crop_traits['aroma'] : $emptyValue;
-									$maturation = isset($traditional_crop_traits['maturation']) ? $traditional_crop_traits['maturation'] : $emptyValue;
-									$pest_and_disease_resistance = isset($traditional_crop_traits['pest_and_disease_resistance']) ? $traditional_crop_traits['pest_and_disease_resistance'] : $emptyValue;
+									$province_name = isset($location['province_name']) ? $location['province_name'] : $emptyValue;
+									$municipality_name = isset($location['municipality_name']) ? $location['municipality_name'] : $emptyValue;
+									$latitude = isset($location['latitude']) ? $location['latitude'] : $emptyValue;
+									$longtitude = isset($location['longtitude']) ? $location['longtitude'] : $emptyValue;
+									$input_date = isset($location['input_date']) ? $location['input_date'] : $emptyValue;
+
 								?>
 									<div class="row">
 										<div class="col-2">
-											<!-- taste -->
-											<label for="taste">Taste</label>
-											<input id="taste" name="taste" type="text" value="<?= $taste; ?>" class="form-control mb-2" disabled>
+											<!-- Province Name -->
+											<label for="province_name">Province Name</label>
+											<input id="province_name" name="province_name" type="text" value="<?= $province_name; ?>" class="form-control mb-2" disabled>
 										</div>
 										<div class="col-2">
-											<!-- aroma -->
-											<label for="aroma">Aroma</label>
-											<input id="aroma" name="aroma" type="text" value="<?= $aroma; ?>" class="form-control mb-2" disabled>
+											<!-- MUnicipality Name -->
+											<label for="municipality_name">MUnicipality Name</label>
+											<input id="municipality_name" name="municipality_name" type="text" value="<?= $municipality_name; ?>" class="form-control mb-2" disabled>
 										</div>
 										<div class="col-2">
-											<!-- maturation -->
-											<label for="matur">Maturation</label>
-											<input id="matur" name="maturation" type="text" value="<?= $maturation; ?>" class="form-control mb-2" disabled>
+											<!-- Latitude -->
+											<label for="latitude">Latitude</label>
+											<input id="latitude" name="latitude" type="text" value="<?= $latitude; ?>" class="form-control mb-2" disabled>
 										</div>
 										<div class="col">
-											<!-- disease resistance -->
-											<label for="resist">Pest and Disease Resistance</label>
-											<input id="resist" name="pest_and_disease_resistance" type="text" value="<?= $pest_and_disease_resistance; ?>" class="form-control" disabled>
+											<!-- Longtitude -->
+											<label for="longtitude">Longtitude</label>
+											<input id="longtitude" name="longtitude" type="text" value="<?= $longtitude; ?>" class="form-control" disabled>
+										</div>
+
+										<?php
+										// Convert the string to a DateTime object
+										$date = new DateTime($input_date);
+										// Format the date to display up to the minute
+										$formatted_date = $date->format('Y-m-d H:i');
+										?>
+										<div class="col">
+											<!-- Input Date -->
+											<label for="input_date">Input Date</label>
+											<input id="input_date" name="input_date" type="text" value="<?= $formatted_date; ?>" class="form-control" disabled>
 										</div>
 										<?php
 									}
 										?>
 								</div>
-
-							<!-- More -->
-							<h3 class="mt-4">More</h5>
-
-							<!-- Planting Techniques -->
-							<label class="mt-2">Planting Techniques</label>
-							<div class="row">
-								<div class="col">
-									<!-- Descrition -->
-									<textarea name="planting_techniques" id="tech-desc" class="form-control" rows="2" disabled <?php echo ($planting_techniques !== $emptyValue) ? '>' . $planting_techniques : 'placeholder="Empty">'; ?></textarea>
-							</div>
-						</div>
-
-						<!-- Cultural and Spiritual Significance -->
-						<label class="mt-2">Cultural and Spiritual Significance</label>
-						<div class="row">
-							<div class="col">
-								<!-- Descrition -->
-								<textarea name="cultural_and_spiritual_significance" id="signif-desc" class="form-control" rows="2" disabled <?php echo ($cultural_and_spiritual_significance !== $emptyValue) ? '>' . $cultural_and_spiritual_significance : 'placeholder="Empty">'; ?></textarea>
-								</div>
-							</div>
-
-							<!-- Role in Maintaining Upland Ecosystems -->
-							<label class="mt-2">Role in Maintaining Upland Ecosystems</label>
-							<div class="row">
-								<div class="col">
-									<!-- Descrition -->
-									<textarea name="role_in_maintaining_upland_ecosystem" id="role-desc" class="form-control" rows="2" disabled <?php echo ($role_in_maintaining_upland_ecosystem !== $emptyValue) ? '>' . $role_in_maintaining_upland_ecosystem : 'placeholder="Empty">'; ?></textarea>
-							</div>
-						</div>
-
-						<!-- Cultural Importance and Traditional Knowledge -->
-						<label class="mt-2">Cultural Importance and Traditional Knowledge</label>
-						<div class="row">
-							<div class="col">
-								<!-- Descrition -->
-								<textarea name="cultural_importance_and_traditional_knowledge" id="impotance-desc" class="form-control" rows="2" disabled <?php echo ($cultural_importance_and_traditional_knowledge !== $emptyValue) ? '>' . $cultural_importance_and_traditional_knowledge : 'placeholder="Empty">'; ?></textarea>
-								</div>
-							</div>
-
-							<!-- Unique Features -->
-							<label class="mt-2">Unique Features</label>
-							<div class="row">
-								<div class="col">
-									<!-- Descrition -->
-									<textarea name="unique_features" id="feat-desc" class="form-control" rows="2" disabled <?php echo ($unique_features !== $emptyValue) ? '>' . $unique_features : 'placeholder="Empty">'; ?></textarea>
-							</div>
-						</div>
-
-						<!-- Cultural Use -->
-						<label class="mt-2">Cultural Use</label>
-						<div class="row">
-							<div class="col">
-								<!-- Descrition -->
-								<textarea name="cultural_use" id="use-desc" class="form-control" rows="2" disabled <?php echo ($cultural_use !== $emptyValue) ? '>' . $cultural_use : 'placeholder="Empty">'; ?></textarea>
-								</div>
-							</div>
 
 							<!-- Associated Farming Practice -->
 							<label class="mt-2">Associated Farming Practice</label>
@@ -275,55 +230,8 @@ require('../sidebar/side.php');
 								<div class="col">
 									<!-- Descrition -->
 									<textarea name="" id="prac-desc" class="form-control" rows="2" disabled ></textarea>
-							</div>
-						</div>
-
-						<!-- Associated Vegetation -->
-						<label class="mt-2">Associated Vegetation</label>
-						<div class="row">
-							<div class="col">
-								<!-- Descrition -->
-								<textarea name="associated_vegetation" id="veg-desc" class="form-control" rows="2" disabled <?php echo ($associated_vegetation !== $emptyValue) ? '>' . $associated_vegetation : 'placeholder="Empty">'; ?></textarea>
 								</div>
 							</div>
-
-							<!-- Last Seen Location -->
-							<label class="mt-2">Last Seen Location (Municipality)</label>
-							<div class="row">
-								<div class="col">
-								<select id="last_seen_location" name="last_seen_location" class="form-select mb-2" disabled>
-									<option value="alabel" <?php echo ($last_seen_location === 'alabel') ? 'selected' : ''; ?>>Alabel</option>
-									<option value="glan" <?php echo ($last_seen_location === 'glan') ? 'selected' : ''; ?>>Glan</option>
-									<option value="kiamba" <?php echo ($last_seen_location === 'kiamba') ? 'selected' : ''; ?>>Kiamba</option>
-									<option value="maasim" <?php echo ($last_seen_location === 'maasim') ? 'selected' : ''; ?>>Maasim</option>
-									<option value="maitum" <?php echo ($last_seen_location === 'maitum') ? 'selected' : ''; ?>>Maitum</option>
-									<option value="malapatan" <?php echo ($last_seen_location === 'malapatan') ? 'selected' : ''; ?>>Malapatan</option>
-									<option value="malungon" <?php echo ($last_seen_location === 'malungon') ? 'selected' : ''; ?>>Malungon</option>
-								</select>							
-								</div>
-							</div>
-
-							<!-- Province -->
-								<!-- <label class="mt-2">Province</label>
-								<div class="row">
-									<div class="col">
-										<select id="province" name="province" class="form-select mb-2">
-											<option value="sarangani" selected>Alabel</option>
-											<option value="davao">Davao</option>
-											<option value="south_cotabato">South Cotabato</option>
-											<option value="cotabato">Cotabato</option>
-										</select>
-									</div>
-								</div> -->
-
-							<!-- Threats to Upland Farms -->
-							<label class="mt-2">Threats to Upland Farms</label>
-							<div class="row">
-								<div class="col">
-									<!-- Descrition -->
-									<textarea name="threats" id="threat-desc" class="form-control" rows="2" disabled <?php echo ($threats !== $emptyValue) ? '>' . $threats : 'placeholder="Empty">'; ?></textarea>
-							</div>
-						</div>
 
 						<!-- Other Information -->
 						<label class="mt-2">Other Information</label>
