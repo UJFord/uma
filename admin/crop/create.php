01cc6805f2cda99deeaa9f92cb61c6a5881cf3ca
UJFord <ujford@gmail.com>
2024-01-14 02:04:09 +0800

    add all entries from redesigning

--- admin/crop/create.php
+++ admin/crop/create.php
@@ -6,11 +6,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous" />
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
-	
+
 	<!-- add entry custom css -->
 	<link rel="stylesheet" href="../../css/admin/entry.css" />
 	<!-- sidebar custom css -->
-	<link rel="stylesheet" href="../../css/admin/side.css"> 
+	<link rel="stylesheet" href="../../css/admin/side.css">
 
 	<!-- favicon -->
 	<link rel="shortcut icon" href="img/logo/Uma logo.svg" type="image/x-icon" />
@@ -53,7 +53,7 @@
 				<div class="form-control p-3 mt-3">
 
 					<!-- general information -->
-					<h3>General Info <span class="fs-6">(Required)</span></h3>
+					<h3 class="fw-bold">General Info <span class="fs-6 fw-semibold">(Required)</span></h3>
 					<div class="row">
 						<div class="col-4">
 							<!-- crop name -->
@@ -69,11 +69,11 @@
 
 					<div class="row mb-4">
 						<div class="col-4">
-							
+
 							<!-- local name -->
 							<label for="crop_local_name">Local Name <span class="text-danger">*</span></label>
 							<input id="crop_local_name" type="text" name="crop_local_name" class="form-control mb-4" required>
-							
+
 							<!-- category -->
 							<label for="category">Category <span class="text-danger">*</span></label>
 							<div class="mb-4">
@@ -119,99 +119,225 @@
 
 					<div class="col">
 						<!-- Descrition -->
-						<label for="gen-desc">Description <span class="text-danger">*</span></label>
+						<label for="gen-desc">Description</label>
 						<textarea name="crop_description" id="gen-desc" class="txtarea form-control" rows="3" required></textarea>
 					</div>
 
 					<!-- More -->
-					<h3 class="mt-5">Additional Info</h5>
 
-						<div class="row">
-							<!-- Location -->
-							<h5 class="mt-2">Location</h5>
-							<div class="col-4">
-								<!-- Municipality -->
-								<label for="municipality">Municipality</label>
-								<select id="municipality" name="municipality_name" class="form-select mb-2">
-									<option value="alabel" selected>None</option>
-									<option value="alabel">Alabel</option>
-									<option value="glan">Glan</option>
-									<option value="kiamba">Kiamba</option>
-									<option value="maasim">Maasim</option>
-									<option value="maitum">Maitum</option>
-									<option value="malapatan">Malapatan</option>
-									<option value="malungon">Malungon</option>
-								</select>
+						<!-- Characteristics -->
+						<h4 class="mt-5">Characteristics</h4>
+							<div class="row">
+								<div class="col-2">
+									<!-- taste -->
+									<label for="taste">Taste</label>
+									<input id="taste" type="text" class="form-control mb-4">
+								</div>
+								<div class="col-2">
+									<!-- aroma -->
+									<label for="aroma">Aroma</label>
+									<input id="aroma" type="text" class="form-control mb-4">
+								</div>
+								<div class="col-2">
+									<!-- maturation -->
+									<label for="matur">Maturation</label>
+									<input id="matur" type="text" class="form-control mb-4">
+								</div>
+								<div class="col">
+									<!-- disease resistance -->
+									<label for="resist">Disease Resistance</label>
+									<input id="resist" type="text" class="form-control">
+								</div>
 							</div>
-							<div class="col-3">
-								<!-- Province -->
-								<label for="province">Province</label>
-								<select id="province" name="province_name" class="form-select mb-2">
-									<option value="sarangani" selected>None</option>
-									<option value="sarangani">Davao Del Norte</option>
-									<option value="davao">Davao</option>
-									<option value="south_cotabato">South Cotabato</option>
-									<option value="cotabato">Cotabato</option>
-								</select>
-							</div>
-							<div class="col-3">
-								<!-- Longtitude -->
-								<label for="longtitude">Longtitude</label>
-								<input id="longtitude" type="text" name="longtitude" placeholder="Enter Longtitude" class="form-control mb-2">
-							</div>
-							<div class="col-2">
-								<!-- Latitude -->
-								<label for="latitude">Latitude</label>
-								<input id="latitude" type="text" name="latitude" placeholder="Enter Latitude" class="form-control">
-							</div>
-						</div>
 
-						<div class="">
-							<!-- Associated Farming Practice -->
-							<h5 class="mt-4">Associated Farming Practice</h5>
-							<div class="col">
-								<!-- Other Info Type -->
-								<label for="farming_practice_type">Type</label>
-								<input id="farming_practice_type" type="text" name="farming_practice_type" placeholder="Enter Farming Practice Type" class="form-control mb-2">
-							</div>
-							<div class="col">
-								<!-- Other Info Name -->
-								<label for="farming_practice_name">Name</label>
-								<input id="farming_practice_name" type="text" name="farming_practice_name" placeholder="Enter Farming Practice Name" class="form-control mb-2">
-							</div>
-							<div class="col">
-								<!-- Other Info Description -->
-								<label for="farming_practice-desc">Description <span class="text-danger"></span></label>
-								<textarea name="farming_practice_description" id="farming_practice-desc" class="txtarea form-control" rows="3"></textarea>
-							</div>
-						</div>
+							<!-- More -->
+							<h4 class="mt-4">Additional Info</h5>
 
-						<!-- Other Information -->
-						<div class="other_info">
-							<h3 class="mt-4 d-flex align-items-center" id="otherInfoTitle">Other Info <i class='bx bx-plus ml-2' id="toggleOtherInfo" style="color: blue;"></i> <i class='bx bx-minus ml-2' id="toggleOtherInfoMinus" style="display: none; color:red"></i></h3>
-							<div class="other-info-content" hidden>
-								<div class="col">
-									<!-- Other Info Type -->
-									<label for="other_info_type">Type</label>
-									<input id="other_info_type" type="text" name="other_info_type" placeholder="Enter Other Info Type" class="form-control mb-2">
+								<!-- Planting Techniques -->
+								<label class="mt-2">Planting Techniques</label>
+								<div class="row">
+									<div class="col">
+										<!-- Descrition -->
+										<textarea name="" id="tech-desc" class="txtarea form-control" rows="2"></textarea>
+									</div>
 								</div>
-								<div class="col">
-									<!-- Other Info Name -->
-									<label for="other_info_name">Name</label>
-									<input id="other_info_name" type="text" name="other_info_name" placeholder="Enter Other Info Name" class="form-control mb-2">
+
+								<!-- Cultural and Spiritual Significance -->
+								<label class="mt-2" for="signif-desc">Cultural and Spiritual Significance</label>
+								<div class="row">
+									<div class="col">
+										<!-- Descrition -->
+										<textarea name="" id="signif-desc" class="txtarea form-control" rows="2"></textarea>
+									</div>
 								</div>
-								<div class="col">
-									<!-- Other Info Description -->
-									<label for="other_info-desc">Description <span class="text-danger"></span></label>
-									<textarea name="other_info_description" id="other_info-desc" class="txtarea form-control" rows="3"></textarea>
+
+								<!-- Role in Maintaining Upland Ecosystems -->
+								<label class="mt-2">Role in Maintaining Upland Ecosystems</label>
+								<div class="row">
+									<div class="col">
+										<!-- Descrition -->
+										<textarea name="" id="role-desc" class="txtarea form-control" rows="2"></textarea>
+									</div>
 								</div>
-								<div class="col">
-									<!-- Other Info Url -->
-									<label for="other_info_url">Links</label>
-									<input id="other_info_url" type="text" name="other_info_url" placeholder="Enter links if available" class="form-control">
+
+								<!-- Cultural Importance and Traditional Knowledge -->
+								<label class="mt-2">Cultural Importance and Traditional Knowledge</label>
+								<div class="row">
+									<div class="col">
+										<!-- Descrition -->
+										<textarea name="" id="impotance-desc" class="txtarea form-control" rows="2"></textarea>
+									</div>
 								</div>
-							</div>
-						</div>
+
+								<!-- Unique Features -->
+								<label class="mt-2">Unique Features</label>
+								<div class="row">
+									<div class="col">
+										<!-- Descrition -->
+										<textarea name="" id="feat-desc" class="txtarea form-control" rows="2"></textarea>
+									</div>
+								</div>
+
+								<!-- Cultural Use -->
+								<label class="mt-2">Cultural Use</label>
+								<div class="row">
+									<div class="col">
+										<!-- Descrition -->
+										<textarea name="" id="use-desc" class="txtarea form-control" rows="2"></textarea>
+									</div>
+								</div>
+
+								<!-- Associated Farming Practice -->
+								<label class="mt-2">Associated Farming Practice</label>
+								<div class="row">
+									<div class="col">
+										<!-- Descrition -->
+										<textarea name="" id="prac-desc" class="txtarea form-control" rows="2"></textarea>
+									</div>
+								</div>
+
+								<!-- Associated Vegetation -->
+								<label class="mt-2">Associated Vegetation</label>
+								<div class="row">
+									<div class="col">
+										<!-- Descrition -->
+										<textarea name="" id="veg-desc" class="txtarea form-control" rows="2"></textarea>
+									</div>
+								</div>
+
+								<!-- Last Seen Location -->
+								<label class="mt-2">Last Seen Location</label>
+								<div class="row">
+									<div class="col">
+										<!-- Descrition -->
+										<textarea name="" id="loc-desc" class="txtarea form-control" rows="2"></textarea>
+									</div>
+								</div>
+
+								<!-- Threats to Upland Farms -->
+								<label class="mt-2">Threats to Upland Farms</label>
+								<div class="row">
+									<div class="col">
+										<!-- Descrition -->
+										<textarea name="" id="threat-desc" class="txtarea form-control" rows="2"></textarea>
+									</div>
+								</div>
+
+								<!-- Other Information -->
+								<label class="mt-2">Other Information</label>
+								<div class="row">
+									<div class="col">
+										<!-- Description -->
+										<textarea name="" id="more-desc" class="txtarea form-control" rows="2"></textarea>
+									</div>
+								</div>
+
+								<div class="row">
+									<!-- Location -->
+									<h5 class="mt-2">Location</h5>
+									<div class="col-4">
+										<!-- Municipality -->
+										<label for="municipality">Municipality</label>
+										<select id="municipality" name="municipality_name" class="form-select mb-2">
+											<option value="alabel" selected>None</option>
+											<option value="alabel">Alabel</option>
+											<option value="glan">Glan</option>
+											<option value="kiamba">Kiamba</option>
+											<option value="maasim">Maasim</option>
+											<option value="maitum">Maitum</option>
+											<option value="malapatan">Malapatan</option>
+											<option value="malungon">Malungon</option>
+										</select>
+									</div>
+									<div class="col-3">
+										<!-- Province -->
+										<label for="province">Province</label>
+										<select id="province" name="province_name" class="form-select mb-2">
+											<option value="sarangani" selected>None</option>
+											<option value="sarangani">Davao Del Norte</option>
+											<option value="davao">Davao</option>
+											<option value="south_cotabato">South Cotabato</option>
+											<option value="cotabato">Cotabato</option>
+										</select>
+									</div>
+									<div class="col-3">
+										<!-- Longtitude -->
+										<label for="longtitude">Longtitude</label>
+										<input id="longtitude" type="text" name="longtitude" placeholder="Enter Longtitude" class="form-control mb-2">
+									</div>
+									<div class="col-2">
+										<!-- Latitude -->
+										<label for="latitude">Latitude</label>
+										<input id="latitude" type="text" name="latitude" placeholder="Enter Latitude" class="form-control">
+									</div>
+								</div>
+
+								<div class="">
+									<!-- Associated Farming Practice -->
+									<h5 class="mt-4">Associated Farming Practice</h5>
+									<div class="col">
+										<!-- Other Info Type -->
+										<label for="farming_practice_type">Type</label>
+										<input id="farming_practice_type" type="text" name="farming_practice_type" placeholder="Enter Farming Practice Type" class="form-control mb-2">
+									</div>
+									<div class="col">
+										<!-- Other Info Name -->
+										<label for="farming_practice_name">Name</label>
+										<input id="farming_practice_name" type="text" name="farming_practice_name" placeholder="Enter Farming Practice Name" class="form-control mb-2">
+									</div>
+									<div class="col">
+										<!-- Other Info Description -->
+										<label for="farming_practice-desc">Description <span class="text-danger"></span></label>
+										<textarea name="farming_practice_description" id="farming_practice-desc" class="txtarea form-control" rows="3"></textarea>
+									</div>
+								</div>
+
+								<!-- Other Information -->
+								<div class="other_info">
+									<h3 class="mt-4 d-flex align-items-center" id="otherInfoTitle">Other Info <i class='bx bx-plus ml-2' id="toggleOtherInfo" style="color: blue;"></i> <i class='bx bx-minus ml-2' id="toggleOtherInfoMinus" style="display: none; color:red"></i></h3>
+									<div class="other-info-content" hidden>
+										<div class="col">
+											<!-- Other Info Type -->
+											<label for="other_info_type">Type</label>
+											<input id="other_info_type" type="text" name="other_info_type" placeholder="Enter Other Info Type" class="form-control mb-2">
+										</div>
+										<div class="col">
+											<!-- Other Info Name -->
+											<label for="other_info_name">Name</label>
+											<input id="other_info_name" type="text" name="other_info_name" placeholder="Enter Other Info Name" class="form-control mb-2">
+										</div>
+										<div class="col">
+											<!-- Other Info Description -->
+											<label for="other_info-desc">Description <span class="text-danger"></span></label>
+											<textarea name="other_info_description" id="other_info-desc" class="txtarea form-control" rows="3"></textarea>
+										</div>
+										<div class="col">
+											<!-- Other Info Url -->
+											<label for="other_info_url">Links</label>
+											<input id="other_info_url" type="text" name="other_info_url" placeholder="Enter links if available" class="form-control">
+										</div>
+									</div>
+								</div>
 				</div>
 				<!-- editting buttons -->
 				<?php
